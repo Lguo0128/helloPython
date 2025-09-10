0b5a4f5481a0eb992083c33b7e5ef179b2df1844
Linda Guo <275215540@qq.com>
2025-09-10 23:32:55 +0800

    20250910 2332

---
+++ practice/ex05_01.py
@@ -0,0 +1,16 @@
+# 5-1 条件测试：编写一系列条件测试；将每个测试以及你对其结果的预测和实际结
+# 果都打印出来。你编写的代码应类似于下面这样：
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+#  详细研究实际结果，直到你明白了它为何为True或False。
+#  创建至少10个测试，且其中结果分别为True和False的测试都至少有5个。
+
+name = 'Linda'
+print("Is name == 'Linda'? I predict True.")
+print(name == 'Linda')
+
+print("\nIs name == 'Sam'? I predict False.")
+print(name == 'Sam')
---
+++ practice/ex05_02.py
@@ -0,0 +1,28 @@
+# 5-2 更多的条件测试：你并非只能创建10个测试。如果你想尝试做更多的比较，可
+# 再编写一些测试，并将它们加入到conditional_tests.py中。对于下面列出的各种测试，
+# 至少编写一个结果为True和False的测试。
+#  检查两个字符串相等和不等。
+#  使用函数lower()的测试。
+#  检查两个数字相等、不等、大于、小于、大于等于和小于等于。
+#  使用关键字and和or的测试。
+#  测试特定的值是否包含在列表中。
+#  测试特定的值是否未包含在列表中。
+
+strA = 'Good'
+strB = 'Bad'
+strC = 'good'
+
+print(strA.lower() == strC)
+print(strA == strB)
+print(strB != strC)
+
+num1 = 900128
+num2 = 890221
+print(num1 >= num2)
+
+listA = ['apple', 'pear', 'banana']
+fruit = 'apple'
+if fruit in listA:
+    print("listA true: " + fruit)
+else:
+    print("listA false: " + fruit)
