4b65cb1046a4f5470457cc829a5f98767f01aa52
Linda Guo <275215540@qq.com>
2025-09-24 15:32:34 +0800

    learning till p302, chap 15.3.10

---
+++ practice/chap15to17-DataVisualization/rw_visual.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+# rw = RandomWalk()
+# rw.fill_walk()
+
+# plt.scatter(rw.x_values,rw.y_values)
+
+while True:
+    rw = RandomWalk(50000)
+    rw.fill_walk()
+
+    # 设置绘图窗口的尺寸 
+    plt.figure(figsize=(10, 6))
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues)
+
+    # 突出起点、终点
+    plt.scatter(0, 0, c="green", s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c="red", s=100)
+
+    # 隐藏坐标轴
+
+    # plt.axes().get_xaxis().set_visible(False)
+    # plt.axes().get_yaxis().set_visible(False)
+
+    # plt.gca().get_xaxis().set_visible(False)
+    # plt.gca().get_yaxis().set_visible(False)
+
+    plt.axis("off")
+
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == "n":
+        break
